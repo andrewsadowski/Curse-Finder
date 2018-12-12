146ec5c101a6db654bf2fa42e78e9506e3151ce6
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-11 21:15:27 -0800

    added .eslintrc and .gitignore to project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 const readDirAndFile = locale => {
   const dir = fs.readdirSync('./data', 'utf-8');
-  // console.log(dir);
   const isLocaleCovered = dir.includes(locale);
   if (isLocaleCovered) {
     const file = fs.readFileSync(`./data/${locale}`);

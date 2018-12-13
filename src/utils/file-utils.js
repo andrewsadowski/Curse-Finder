d154cd4cfda1797d1b462935281961b0a3ad0dc6
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-12 23:43:41 -0800

    added utils dir, added search.js, added todos in both file-utils and search

---
+++ src/utils/file-utils.js
@@ -0,0 +1,5 @@
+/**
+ * TODO: - Create utility that breaks file into array of words
+ * TODO: - Create utility that orders said words
+ * TODO: - Create utility that write matches to console && file
+ */
---
+++ src/utils/search.js
@@ -0,0 +1,3 @@
+/**
+ * TODO : Create Binary Search function to
+ */

544815a294b9efd8f3d86b0bf766047715b9d9f1
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-13 19:26:57 -0800

    updated todos

--- src/utils/file-utils.js
+++ src/utils/file-utils.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 /**
- * TODO: - Create utility that breaks file into array of words
  * TODO: - Create utility that orders said words
  * TODO: - Create utility that write matches to console && file
  * TODO: - Create utility that takes a locale code and processes file into an array of naughty words

291dbfbeee432948a9d0b2ba1fe6a19e1d8ead63
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-12-24 16:18:12 -0500

    added alpha-sort package, updated sort-utils with getLocaleSpecficFile

--- src/utils/sort-utils.js
+++ src/utils/sort-utils.js
@@ -1,3 +1,22 @@
+const fs = require('fs');
+const alphaSort = require('alpha-sort');
+
+const collator = new Intl.Collator();
+
+const { readFileByLocale, dirToArr } = require('./file-utils');
+
+let compare = (a, b) => (a === b ? 0 : collator.compare(a, b));
+
+const getLocaleSpecificFile = async (pathName, locale) => {
+  const localeArr = await readFileByLocale(locale);
+  console.log(collator('cs'));
+  let sortedArr = await localeArr.sort(alphaSort.asc);
+  console.log(`localeArr: ${localeArr}\n sortedArr: ${sortedArr}`);
+  return sortedArr;
+};
+
+getLocaleSpecificFile('sv');
+
 /**
- * TODO - Sort array with quicksort/mergesort
+ * TODO - Sort array with INTL.colator (locale, array)
  */
